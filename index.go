4c50c292a491305c9933c6d208f133e5553c2f19
JustFr4nck <francyperry07@gmail.com>
2025-09-19 14:15:41 +0200

    convert string to bin added

--- index.go
+++ index.go
@@ -9,15 +9,17 @@ func main() {
 
 	var (
 		val int = 0
-		n   int = 0
+		scr string = "";
+		n int = 0
 	)
 
 	fmt.Print("\n___MENU___")
-	fmt.Print("Inserire i valori richiesti per eseguire le seguenti operazioni")
-	fmt.Print("1) Per trasformare un testo in un codice binario")
-	fmt.Print("2) Per trasformare un valore decimale in binario")
-	fmt.Print("3) Per trasformare un codice binario in un valore numerico o in un testo")
-	fmt.Print("EXIT) Inserire qualsiasi altro valore per uscire dal programma")
+	fmt.Print("\nInserire i valori richiesti per eseguire le seguenti operazioni")
+	fmt.Print("\n1) Per trasformare una parola in codice binario")
+	fmt.Print("\n2) Per trasformare un valore decimale in binario")
+	fmt.Print("\n3) Per trasformare un codice binario in un valore numerico")
+	fmt.Print("\n4) Per trasformare un codice binario in una parola")
+	fmt.Print("\nEXIT) Inserire qualsiasi altro valore per uscire dal programma\n")
 
 	fmt.Scanln(&n)
 
@@ -26,6 +28,17 @@ func main() {
 		switch n {
 
 		case 1:
+			fmt.Print("\nInserisci parola da convertire: ")
+			fmt.Scanln(&scr)
+
+			for _, char := range scr{
+
+				charInt := int(char);
+
+				bin := fmt.Sprintf("%08b", charInt)
+
+				fmt.Print(bin, " ")
+			}
 			
 
 		case 2:
